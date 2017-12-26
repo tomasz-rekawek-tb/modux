3d67e82d817501c3818cb3977b29d477c38dde4d
Vlad <vlad.filip.dev@gmail.com>
2017-12-26 15:35:05 +0200

    fixed looping over nodelists

--- core/scripts/utils/isobject.js
+++ core/scripts/utils/isobject.js
@@ -1,3 +1,3 @@
 'use strict'
 
-module.exports = ( o ) => !Array.isArray( o ) && o !== null && typeof o === 'object' && o.constructor && o.constructor.name === 'Object'
+module.exports = o => !Array.isArray( o ) && o !== null && typeof o === 'object' && o.constructor && o.constructor.name === 'Object'
--- core/scripts/utils/loop.js
+++ core/scripts/utils/loop.js
@@ -1,17 +1,9 @@
 'use strict'
 
-const isObject = require( __dirname + '/isobject' )
-
 module.exports = ( collection, cb ) => {
-  if ( isObject( collection ) ) {
+  if ( collection ) {
     for ( let i = 0, k = Object.keys( collection ), l = k.length; i < l; i++ ) {
       cb( collection[ k[ i ] ], k[ i ] )
-    }
-    return
-  }
-  if ( Array.isArray( collection ) ) {
-    for ( let i = 0, l = collection.length; i < l; i++ ) {
-      cb( collection[ i ], i )
     }
     return
   }
