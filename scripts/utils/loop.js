afb101c252619078391e59e1129a938b38987464
Vlad Filip <vlad.filip.devl@gmail.com>
2018-03-03 14:08:45 +0200

    You can now break out of loops with a return false

--- scripts/utils/loop.js
+++ scripts/utils/loop.js
@@ -3,7 +3,10 @@
 module.exports = ( collection, cb ) => {
   if ( collection ) {
     for ( let i = 0, k = Object.keys( collection ), l = k.length; i < l; i++ ) {
-      cb( collection[ k[ i ] ], Array.isArray( collection ) ? parseInt( k[ i ] ) : k[ i ] )
+      let result = !!cb( collection[ k[ i ] ], Array.isArray( collection ) ? parseInt( k[ i ] ) : k[ i ] )
+      if ( result === false ) {
+        break
+      }
     }
     return
   }
