0692ffab6378fb86d9ef4b60ff825520f3841b55
Vlad Filip <vlad.filip.devl@gmail.com>
2018-03-03 14:09:45 +0200

    loop break allows empty return as true

--- scripts/utils/loop.js
+++ scripts/utils/loop.js
@@ -3,7 +3,7 @@
 module.exports = ( collection, cb ) => {
   if ( collection ) {
     for ( let i = 0, k = Object.keys( collection ), l = k.length; i < l; i++ ) {
-      let result = !!cb( collection[ k[ i ] ], Array.isArray( collection ) ? parseInt( k[ i ] ) : k[ i ] )
+      let result = cb( collection[ k[ i ] ], Array.isArray( collection ) ? parseInt( k[ i ] ) : k[ i ] )
       if ( result === false ) {
         break
       }
