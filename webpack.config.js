426530883c8cad703917801013c369681169891f
Vlad Filip <vlad.filip.devl@gmail.com>
2018-03-14 18:04:27 +0200

    fixed babel use

--- webpack.config.js
+++ webpack.config.js
@@ -61,6 +61,20 @@ module.exports = () => {
       loaders: [
         {
           test: /\.js$/,
+          //exclude: [ /node_modules/ ],
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                presets: [ 
+                  "env"
+                ]
+              }
+             }
+          ]
+        },
+        {
+          test: /\.js$/,
           loader: 'babel-loader',
           exclude: [ /node_modules/ ],
           include: [ __dirname ],
