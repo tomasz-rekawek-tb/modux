72a863982e17ecbe47e316e90916de880e5b73c9
Vlad Filip <vlad.filip.devl@gmail.com>
2018-04-16 12:32:48 +0300

    Added IE edge support for console info

--- scripts/utils/logger.js
+++ scripts/utils/logger.js
@@ -14,25 +14,25 @@ module.exports = {
   },
 
   log: function () {
-    if ( debug && console ) {
+    if ( debug && console && console.log ) {
       console.log.apply( this, [ id, ...arguments ] )
     }
   },
 
   info: function () {
-    if ( debug && console ) {
+    if ( debug && console && console.info ) {
       console.info.apply( this, [ id, ...arguments ] )
     }
   },
 
   warn: function () {
-    if ( debug && console ) {
+    if ( debug && console && console.warn ) {
       console.warn.apply( this, [ id, ...arguments ] )
     }
   },
 
   error: function () {
-    if ( debug && console ) {
+    if ( debug && console && console.error ) {
       console.error.apply( this, [ id, ...arguments ] )
     }
   }
