9c9a1f80a4f563415fcd62f32a67de018a0e28f5
Vlad Filip <vlad.filip.devl@gmail.com>
2018-09-11 14:33:31 +0200

    code alignment

--- scripts/utils/approx.js
+++ scripts/utils/approx.js
@@ -1,6 +1,6 @@
 'use strict'
 
-module.exports = ( num, decimals ) =>{
-    let power = Math.pow( 10, decimals )
-    return Math.floor( num * power ) / power
+module.exports = ( num, decimals ) => {
+  let power = Math.pow( 10, decimals )
+  return Math.floor( num * power ) / power
 }
