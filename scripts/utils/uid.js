e370430c294906a26d4f1e960ed2d6187383e743
Vlad Filip <vlad.filip.devl@gmail.com>
2018-10-22 17:31:31 +0300

    fixed uuid

--- scripts/utils/uid.js
+++ scripts/utils/uid.js
@@ -1,19 +1,15 @@
 'use strict'
 
-let cache = []
+let cache = 0
 let lastReset = Date.now()
 
 const uid = () => {
   if ( lastReset !== Date.now() ) {
-    cache.length = 0
+    cache = 0
+    lastReset = Date.now()
   }
-  let id = Date.now() + '' + Math.random().toString().substr( 2 )
-  for ( let i = 0; i < cache.length; i++ ) {
-    if ( cache[ i ] === id ) {
-      return uid()
-    }
-  }
-  return id
+  cache++
+  return Date.now() + '' + cache
 }
 
 module.exports = uid
