ee0b184bc83ca91f73260622d138a06ea661a47a
Vlad Filip <vlad.filip.devl@gmail.com>
2019-02-06 11:24:18 +0200

    fixed approx

--- scripts/utils/approx.js
+++ scripts/utils/approx.js
@@ -4,8 +4,8 @@
  * Used to approximate a number to a certain number of decimals
  * @param {Numeric} num The number to be approximated
  * @param {Numeric} decimals The number of decimals to approximate to
- * @return {String} A new number
+ * @return {Numeric} A new number
  */
 export let approx = ( num, decimals ) => {
-  return parseFloat( num ).toFixed( decimals )
+  return parseFloat( parseFloat( num ).toFixed( decimals ) )
 }
