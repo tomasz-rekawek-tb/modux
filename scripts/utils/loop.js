e696c0571de5b88909589d4c7e3f8b28310d7b08
Vlad Filip <vlad.filip.devl@gmail.com>
2019-04-13 12:18:37 +0300

    updated loop to break on value return

--- scripts/utils/loop.js
+++ scripts/utils/loop.js
@@ -1,18 +1,17 @@
 'use strict'
 
 /**
- * Loop through a collection Object or Array. Return false from callback to cancel the loop
+ * Loop through a collection Object or Array. If the function returns a value other then undefined, the loop stops.
  * @param {Object|Array} collection The collection to loop through
  * @param {Function} cb The callback which is called for every element in collection
  */
-export let loop = ( collection, cb ) => {
+module.exports = ( collection = {}, cb ) => {
   if ( collection ) {
     for ( let i = 0, k = Object.keys( collection ), l = k.length; i < l; i++ ) {
       let result = cb( collection[ k[ i ] ], Array.isArray( collection ) ? parseInt( k[ i ] ) : k[ i ] )
-      if ( result === false ) {
-        break
+      if ( result !== undefined ) {
+        return result
       }
     }
-    return
   }
 }
